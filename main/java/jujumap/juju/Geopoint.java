d6912267b2312741004f1c6ab64af06b4bca2258
jmages <jmages@gmx.de>
2014-04-13 11:04:37 +0200

    cleaning up Geopoint inheritance

--- main/java/jujumap/juju/Geopoint.java
+++ main/java/jujumap/juju/Geopoint.java
@@ -7,12 +7,6 @@ public class Geopoint {
 
     String name     = "";
 
-    String url      = "";
-    String ortsInfo = "";
-    String geo      = "";
-    String roadInfo = "";
-    String km       = "";
-
     float lon;
     float lat;
     float alt;
@@ -22,27 +16,11 @@ public class Geopoint {
         splitRawTrackPoint (rawTrackPoint);
     }
 
-    public Geopoint (String name, String rawTrackPoint, String description) {
+    public Geopoint (String name, String rawTrackPoint) {
 
         this.name = name;
 
         splitRawTrackPoint (rawTrackPoint);
-
-        splitDescription (description);
-    }
-
-    private void splitDescription(String description) {
-
-        ArrayList <String> rawInfo = new ArrayList <String> (Arrays.asList (description.split ("<!--")));
-
-        if (rawInfo.size() == 5) {
-
-            url      = rawInfo.get(0);
-            ortsInfo = rawInfo.get(1).substring(0, rawInfo.get(1).length()-8).trim();
-            geo      = rawInfo.get(2).substring(0, rawInfo.get(2).length()-8).trim();
-            roadInfo = rawInfo.get(3).substring(0, rawInfo.get(3).length()-8).trim();
-            km       = rawInfo.get(4).substring(0, rawInfo.get(4).length()-3).trim();
-        }
     }
 
     public Geopoint (double lat, double lon) {
--- main/java/jujumap/juju/PlacePoint.java
+++ main/java/jujumap/juju/PlacePoint.java
@@ -1,9 +1,35 @@
 package jujumap.juju;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class PlacePoint extends Geopoint {
+
+    String url      = "";
+    String ortsInfo = "";
+    String geo      = "";
+    String roadInfo = "";
+    String km       = "";
 
     public PlacePoint (String name, String rawTrackPoint, String description) {
 
-        super(name, rawTrackPoint, description);
+        super(name, rawTrackPoint);
+
+        splitDescription (description);
+
+    }
+
+    private void splitDescription(String description) {
+
+        ArrayList<String> rawInfo = new ArrayList <String> (Arrays.asList(description.split("<!--")));
+
+        if (rawInfo.size() == 5) {
+
+            url      = rawInfo.get(0);
+            ortsInfo = rawInfo.get(1).substring(0, rawInfo.get(1).length()-8).trim();
+            geo      = rawInfo.get(2).substring(0, rawInfo.get(2).length()-8).trim();
+            roadInfo = rawInfo.get(3).substring(0, rawInfo.get(3).length()-8).trim();
+            km       = rawInfo.get(4).substring(0, rawInfo.get(4).length()-3).trim();
+        }
     }
 }
